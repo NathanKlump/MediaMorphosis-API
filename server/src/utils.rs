84222bb8ed45f36e174f271541fa70685991372b
nathanklump <nathanieljklump@gmail.com>
2023-11-22 21:20:32 -0500

    server client

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -7,14 +7,14 @@ use std::io::Write;
 use uuid::Uuid;
 
 pub async fn handle_file_upload(payload: &mut web::Payload) -> Result<(), Error> {
+    let uuid_v4 = Uuid::new_v4();
+    let uuid_string = uuid_v4.to_string();
+    
     while let Some(chunk) = payload.next().await {
         let data = chunk?;
         // Process chunks here; for example, write them to a file
-        let uuid_v4 = Uuid::new_v4();
-        let uuid_string = uuid_v4.to_string();
-        let mut file = web::block(std::fs::File::create(|| uuid_string)).await??;
+        let mut file = web::block(|| std::fs::File::create(&uuid_string)).await??;
         file.write_all(&data)?;
     }
-
     Ok(())
 }
