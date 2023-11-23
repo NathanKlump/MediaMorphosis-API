6cef2d25b2849ab9c48e3ce026ab805f1ada8b23
johnAtaalla <johnataalla@gmail.com>
2023-11-22 19:45:24 -0500

    Added random name generation for uploaded files

--- src/utils.rs
+++ src/utils.rs
@@ -4,12 +4,15 @@
 use actix_web::{web, Error};
 use futures::StreamExt;
 use std::io::Write;
+use uuid::Uuid;
 
 pub async fn handle_file_upload(payload: &mut web::Payload) -> Result<(), Error> {
     while let Some(chunk) = payload.next().await {
         let data = chunk?;
         // Process chunks here; for example, write them to a file
-        let mut file = web::block(|| std::fs::File::create("uploaded_file")).await??;
+        let mut uuid_v4 = Uuid::new_v4();
+        let uuid_string = uuid_v4.to_string();
+        let mut file = web::block(|| std::fs::File::create(uuid_string)).await??;
         file.write_all(&data)?;
     }
 
