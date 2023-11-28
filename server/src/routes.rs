207f8a7c5925552e91dd11625b98aa62405500c9
johnAtaalla <johnataalla@gmail.com>
2023-11-28 18:18:15 -0500

    added file retreival

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -6,17 +6,32 @@ use actix_multipart::Multipart;
 use actix_web::HttpResponse;
 use crate::utils;
 use crate::converter;
+use std::fs::File;
+use std::io::Read;
+use std::fs;
 
 pub async fn upload_file(payload: Multipart) -> HttpResponse {
     let result = utils::handle_file_upload(payload).await;
 
     match result {
         Ok((file_name, output_name)) => {
-            match converter::convert_file(file_name, output_name) {
-                Ok(()) => HttpResponse::Ok().body("File uploaded and conversion successful"),
+            match converter::convert_file(file_name.clone(), output_name.clone()) {
+                Ok(()) => {
+                    // Read the converted file and return it in the response
+                    let mut file = File::open(output_name.clone()).expect("Failed to open converted file");
+                    let mut contents = Vec::new();
+                    file.read_to_end(&mut contents).expect("Failed to read converted file");
+
+                    HttpResponse::Ok()
+                        .header("Content-Disposition", format!("attachment; filename=\"{}\"", output_name))
+                        .content_type("application/octet-stream")
+                        .body(contents)
+                },
                 Err(e) => HttpResponse::InternalServerError().body(format!("File uploaded but conversion failed: {}", e)),
             }
         },
         Err(e) => HttpResponse::InternalServerError().body(format!("File upload failed: {}", e)),
     }
+    //fs::remove_file(output_name)?;
+
 }
