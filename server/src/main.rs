a0e84492a33dd2b5e634ade7dc0162d78ec63a0b
nathanklump <nathanieljklump@gmail.com>
2023-12-02 17:25:12 -0500

    con

--- server/src/main.rs
+++ server/src/main.rs
@@ -27,7 +27,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             .route("/upload", web::post().to(routes::upload_file))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
